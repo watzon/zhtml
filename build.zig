5618eaaa2f8795280641d709b39636a990b95d32
Ryan Liptak <squeek502@hotmail.com>
2020-07-07 00:11:35 -0700

    Add test runner for html5lib-tests tokenizer tests
    Can be run via `zig build test-html5lib`
    
    - Adds https://github.com/html5lib/html5lib-tests as a submodule in test/html5lib-tests
    - Adds a test runner for the tokenizer test files
      + Note: None of the test files currently pass in their entirety yet; making these tests pass is just something to shoot for
      + Not all of the test files are included yet, since either the test runner doesn't support something yet (e.g. doubleEscaped) or the tokenizer doesn't support something yet (e.g. lastStartTag)

--- build.zig
+++ build.zig
@@ -21,6 +21,14 @@ pub fn build(b: *Builder) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    var html5lib_tests = b.addTest("test/tokenizer-html5lib.zig");
+    html5lib_tests.setBuildMode(mode);
+    html5lib_tests.addPackagePath("zhtml/token", "src/token.zig");
+    html5lib_tests.addPackagePath("zhtml/parse_error", "src/parse_error.zig");
+    html5lib_tests.addPackagePath("zhtml/tokenizer", "src/tokenizer.zig");
+    const html5lib_test_step = b.step("test-html5lib", "Run the tests from html5lib/html5lib-tests");
+    html5lib_test_step.dependOn(&html5lib_tests.step);
 }
 
 // pub fn build(b: *Builder) void {
