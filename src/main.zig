fc950f76859abaf5f3c0cea3bd3d8f0ee3a7b2bd
Ryan Liptak <squeek502@hotmail.com>
2020-07-07 00:02:03 -0700

    Fix/update main.zig
    - Tokenizer.next was renamed to Tokenizer.nextToken in 7a7b2b51e94fdf5aecf836e97989ca79dbb5efb5
    - Also removes the ./ from the path to test.html to make it work on Windows

--- src/main.zig
+++ src/main.zig
@@ -3,17 +3,17 @@ const Tokenizer = @import("tokenizer.zig").Tokenizer;
 
 pub const log_level: std.log.Level = .debug;
 
-pub fn main() !void { 
+pub fn main() !void {
     var alloc = std.heap.page_allocator;
-    var tokenizer = try Tokenizer.initWithFile(alloc, "./test.html");
+    var tokenizer = try Tokenizer.initWithFile(alloc, "test.html");
     while (true) {
-        var token = tokenizer.next() catch |err| {
+        var token = tokenizer.nextToken() catch |err| {
             std.log.err(.main, "{} at line: {}, column: {}\n", .{ err, tokenizer.line, tokenizer.column });
             continue;
         };
 
         if (token) |tok| {
-            std.log.info(.main, "{}\n", .{ tok });
+            std.log.info(.main, "{}\n", .{tok});
             if (tok == .EndOfFile) break;
         }
     }
